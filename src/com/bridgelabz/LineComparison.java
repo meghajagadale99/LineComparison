b9b78600625f470409d4cb3e423aa56e7fb61d41
Megha <meghajagadale99@gmail.com>
2021-08-05 12:21:38 +0530

    Added: checking equality of a length line comparison

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -9,9 +9,21 @@ public class LineComparison
         int x2 = 7;
         int y1 = 8;
         int y2 = 89;
+        int m1 = 14;
+        int m2 = 16;
+        int d1 = 4;
+        int d2 = 85;
 
         double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        double lengthOfLine2 = Math.sqrt((m2 - m1) * (m2 - m1) + (d2 - d1) * (d2 - d1));
+
         System.out.println(lengthOfLine1);
+        System.out.println(lengthOfLine2);
+
+        if(lengthOfLine1 == lengthOfLine2)
+            System.out.println("Lines are equal");
+        else
+            System.out.println("Lines are not equal");
         }
     }
 
