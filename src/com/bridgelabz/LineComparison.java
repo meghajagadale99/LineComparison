9ac5f7dfbcc7c7071cc96b27742cb527deba4795
Megha <meghajagadale99@gmail.com>
2021-08-09 22:33:12 +0530

    Added: compare to using oops

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,31 +1,41 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class LineComparison
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to Line Comparison Computation Program");
-        int x1 = 5;
-        int x2 = 7;
-        int y1 = 8;
-        int y2 = 89;
-        int m1 = 14;
-        int m2 = 19;
-        int d1 = 5;
-        int d2 = 95;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the co-ordinates  for x-axis of First Line: ");
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        System.out.println("Enter the co-ordinates for y-axis of First Line: ");
+        int y1 = sc.nextInt();
+        int y2 = sc.nextInt();
+        System.out.println("Enter the co-ordinates  for x-axis of second Line: ");
+        int m1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        System.out.println("Enter the co-ordinates for y-axis of second Line: ");
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
 
-        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        double lengthOfLine2 = Math.sqrt((m2 - m1) * (m2 - m1) + (d2 - d1) * (d2 - d1));
+        LineComparison Compare = new LineComparison();
+        double lengthOfLine1 = Compare.findLength(x1, x2, y1, y2);
+        System.out.println("The lenth of First Line is :" + lengthOfLine1);
+        double lengthOfLine2 = Compare.findLength(m1, m2, d1, d2);
+        System.out.println("The lenth second Line is :" + lengthOfLine2);
+        Compare.checkEquality(lengthOfLine1, lengthOfLine2);
 
-        System.out.println(lengthOfLine1);
-        System.out.println(lengthOfLine2);
-
-        if(lengthOfLine1>lengthOfLine2)
-            System.out.println("Length of Line1 is greater than Length of Line2");
-        else if(lengthOfLine1<lengthOfLine2)
-            System.out.println("Length of Line1 is lesser than Length of Line2");
+    }
+    public double findLength(int x1, int x2, int y1, int y2) {
+        double lengthofline = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        return lengthofline;
+    }
+    public void checkEquality(double Line1, double Line2) {
+        if (Line1 > Line2)
+            System.out.println("Length of Line 1 is greater than the Length of Line 2");
+        else if (Line1 < Line2)
+            System.out.println("Length of Line 1 is lesser than the Length of Line 2");
         else
-            System.out.println("Both lines are Equal");
-        }
+            System.out.println("Both the lines have equal length");
     }
 
+}
